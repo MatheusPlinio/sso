64035bc67a628ae062ea20b92b6568102f18f51a
Matheus <mplinio13@gmail.com>
2023-09-10 17:51:09 -0300

    toolkit:instalado

--- setup/auth/models.py
+++ setup/auth/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class OAuth2Application(models.Model):
-    name = models.CharField(max_length=255)
-    client_id = models.CharField(max_length=255, unique=True)
-    client_secret = models.CharField(max_length=255, unique=True)
-########## Criação do primeiro modelo ####################
--- setup/auth/views.py
+++ setup/auth/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from oauth2_provider.models import get_application_model
+from auth.serializers import OAuth2ApplicationSerializer
 
-# Create your views here.
+Application = get_application_model()
+
+
+class ApplicationRegistrationView(generics.CreateAPIView):
+    queryset = Application.objects.all()
+    serializer_class = OAuth2ApplicationSerializer
